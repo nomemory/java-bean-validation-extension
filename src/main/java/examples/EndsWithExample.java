662598e10e3b84bdf98ea1a4d85014e409d340c1
Ciobanu, Andrei-Nicolin (UK - EDC) <andreinicolinciobanu@deloitte.co.uk>
2017-10-13 11:21:33 +0300

    Commenting the example file to avoid having main methods in the library code.

--- src/main/java/examples/EndsWithExample.java
+++ src/main/java/examples/EndsWithExample.java
@@ -1,24 +1,24 @@
-package examples;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import net.andreinc.jvbe.annotations.str.EndsWith;
-
-import java.util.List;
-
-import static java.util.Arrays.asList;
-
-public class EndsWithExample {
-    public static void main(String[] args) {
-        SomeStrings someStrings =
-                new SomeStrings(asList("a1", "a2", "b2", "b3"));
-    }
-}
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-class SomeStrings {
-    private List<@EndsWith(value = {"1", "2"}) String> someStrings;
-}
+//package examples;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import net.andreinc.jvbe.annotations.str.EndsWith;
+//
+//import java.util.List;
+//
+//import static java.util.Arrays.asList;
+//
+//public class EndsWithExample {
+//    public static void main(String[] args) {
+//        SomeStrings someStrings =
+//                new SomeStrings(asList("a1", "a2", "b2", "b3"));
+//    }
+//}
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//class SomeStrings {
+//    private List<@EndsWith(value = {"1", "2"}) String> someStrings;
+//}
