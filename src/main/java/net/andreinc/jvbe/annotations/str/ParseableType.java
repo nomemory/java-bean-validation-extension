cecac38beab54a66d321da331a720ca14428f95a
Ciobanu, Andrei-Nicolin (UK - EDC) <andreinicolinciobanu@deloitte.co.uk>
2017-10-18 12:03:03 +0300

    Added new annotation @Parseable (2)

---
+++ src/main/java/net/andreinc/jvbe/annotations/str/ParseableType.java
@@ -0,0 +1,15 @@
+package net.andreinc.jvbe.annotations.str;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public enum ParseableType {
+    TO_SHORT("Short"),
+    TO_INT("Integer"),
+    TO_LONG("Long"),
+    TO_DOUBLE("Double"),
+    TO_FLOAT("Float");
+    @Getter
+    private String friendlyName;
+}
