af133c16a83b6685fe9bd8c2a8b22ef965d7c6b5
Ciobanu, Andrei-Nicolin (UK - EDC) <andreinicolinciobanu@deloitte.co.uk>
2017-10-18 12:02:30 +0300

    Added new annotation @Parseable.

---
+++ src/main/java/net/andreinc/jvbe/annotations/str/Parseable.java
@@ -0,0 +1,26 @@
+package net.andreinc.jvbe.annotations.str;
+
+import net.andreinc.jvbe.annotations.str.validators.ParseableValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Constraint(validatedBy = ParseableValidator.class)
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+public @interface Parseable {
+    ParseableType value();
+
+    String message() default "{string.parseable}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/net/andreinc/jvbe/annotations/str/validators/ParseableValidator.java
@@ -0,0 +1,52 @@
+package net.andreinc.jvbe.annotations.str.validators;
+
+import net.andreinc.jvbe.annotations.str.Parseable;
+import net.andreinc.jvbe.annotations.str.ParseableType;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.function.Function;
+
+public class ParseableValidator implements ConstraintValidator<Parseable, String> {
+
+    private Parseable annotation;
+
+    @Override
+    public void initialize(Parseable constraintAnnotation) {
+        this.annotation = constraintAnnotation;
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+
+        if (null == value) {
+            return false;
+        }
+
+        ParseableType type = annotation.value();
+
+        switch (type) {
+            case TO_INT:
+                return catcher(value, Integer::parseInt);
+            case TO_DOUBLE:
+                return catcher(value, Double::parseDouble);
+            case TO_LONG:
+                return catcher(value, Long::parseLong);
+            case TO_SHORT:
+                return catcher(value, Short::parseShort);
+            case TO_FLOAT:
+                return catcher(value, Float::parseFloat);
+        }
+
+        return false;
+    }
+
+    private <T> boolean catcher(String value, Function<String, T> function) {
+        try {
+            function.apply(value);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
